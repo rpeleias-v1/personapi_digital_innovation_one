dedc27341cdd7d19d9860b585a5cab143be45a45
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-13 09:01:01 -0300

    Created project initial structure

---
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
