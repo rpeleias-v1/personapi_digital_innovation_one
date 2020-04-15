4016f87678952e993912affbd2d558db38fb554a
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-15 10:30:23 -0300

    Developed unit tests for GET by id service method

--- src/test/java/one/digitalinnovation/personapi/services/PersonServiceTest.java
+++ src/test/java/one/digitalinnovation/personapi/services/PersonServiceTest.java
@@ -4,6 +4,7 @@ import one.digitalinnovation.personapi.dto.mapper.PersonMapper;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entities.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,9 +12,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeDTO;
 import static one.digitalinnovation.personapi.utils.PersonUtils.createFakeEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
@@ -41,6 +45,32 @@ public class PersonServiceTest {
         MessageResponseDTO successMessage = personService.create(personDTO);
 
         assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    @Test
+    void testGivenValidPersonIdThenReturnThisPerson() throws PersonNotFoundException {
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+        expectedPersonDTO.setId(expectedSavedPerson.getId());
+
+        when(personRepository.findById(expectedSavedPerson.getId())).thenReturn(Optional.of(expectedSavedPerson));
+        when(personMapper.toDTO(expectedSavedPerson)).thenReturn(expectedPersonDTO);
+
+        PersonDTO personDTO = personService.findById(expectedSavedPerson.getId());
+
+        assertEquals(expectedPersonDTO, personDTO);
+
+        assertEquals(expectedSavedPerson.getId(), personDTO.getId());
+        assertEquals(expectedSavedPerson.getFirstName(), personDTO.getFirstName());
+    }
+
+    @Test
+    void testGivenInvalidPersonIdThenThrowException(){
+        var invalidId = 1L;
+        when(personRepository.findById(invalidId))
+                .thenReturn(Optional.ofNullable(any(Person.class)));
+
+        assertThrows(PersonNotFoundException.class, () -> personService.findById(invalidId));
     }
 
     private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
